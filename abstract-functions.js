a1c72d7c37a03e92da130dfeeaf424853c88ac25
Kowall, Marian (SE-A/31) <marian.kowall@volkswagen.de>
2022-01-28 17:43:25 +0100

    fertig :

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,12 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
+// new Version form the same code marian kowall 
+//Organizing Code with Functions
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+const createPerson = (name, height, favoriteColor) =>  console.log("My name is " + name + " and my height is " + height + " and my favorite color is " + favoriteColor);
 
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
 
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+createPerson("Bob", "72", "red");
+createPerson("Tamera", "63", "blue");
+createPerson("Christopher", "69", "green");
+createPerson("Samantha", "65", "orange");
